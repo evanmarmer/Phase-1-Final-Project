4467dad8a2f8cab05d9fa6075125caff88af1b1e
evanmarmer <evanmarmer@gmail.com>
2023-05-17 09:11:42 -0600

    basic setup printed to DOM

--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ const sonsUrl = "http://localhost:3000/seasons"
 function getAllSeasons(){
     fetch(sonsUrl)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displaySeasons(data))
 }
 getAllSeasons()
 // asynchronous function (promise) that allows the rest of the code to be loaded before the response (data retrieval) from the json server happens
 
+function renderCard(season){
+    // console.log(season)
+    const seasonCard = document.createElement('div')
+    seasonCard.classname = 'card'
+    seasonCard.innerHTML = `
+    <img src="${season.image.original}">
+    <div class ="content">
+        <h1>Season ${season.number}</h1>
+        <h2>Premier Date ${season.premiereDate}</h2>
+        </div>
+    `
+    document.querySelector('#season-list').appendChild(seasonCard)
+}
 
 
-
-
-
+function displaySeasons(seasons){
+    // console.log(seasons)
+    seasons.forEach(season => {
+        renderCard(season)
+        // console.log(season)
+    })
+}
 
 
